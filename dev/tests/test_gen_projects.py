fef3c3c90305598240f552e668b09756664117cd
Li Lin <guru.lin@gmail.com>
2024-06-24 04:06:57 +0800

    tested manual project generation

--- dev/tests/test_gen_projects.py
+++ dev/tests/test_gen_projects.py
@@ -1,4 +1,6 @@
+import yaml
 import os
+import shutil
 from pathlib import Path
 from typing import Sequence
 
@@ -12,8 +14,15 @@ template_path = os.path.abspath(os.path.join(cwd, "../.."))
 def test_rendered_project(tmp_path: Path):
     dst_path = "/Users/lee/tmp/ttt"
     if os.path.exists(dst_path):
-        os.rmdir(dst_path)
+        shutil.rmtree(dst_path)
 
-    result = run_copy(template_path, dst_path)
+    answer_data = read_answer(cwd + "/fastapi_async.yml")
+    result = run_copy(template_path, dst_path, data=answer_data)
 
     print(result)
+
+
+def read_answer(file_path):
+    with open(file_path, "r") as file:
+        data = yaml.load(file, Loader=yaml.FullLoader)
+    return data
